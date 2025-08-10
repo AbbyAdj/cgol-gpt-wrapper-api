3e41c222e009477f78c6e02b719a17108d62a9fd
AbbyAdj <abbyadjei9@gmail.com>
2025-08-10 22:36:48 +0100

    separate html, css and js

---
+++ api/static/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("cgolForm");
+const responseRenderArea = document.getElementById("responseRenderArea");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  document.getElementById("submit_button").classList.add("loading");
+  const formData = new FormData(form);
+  const userInput = formData.get("user_input");
+
+  const response = await fetch("/results", {
+    method: "POST",
+    body: formData,
+  });
+  document.getElementById("submit_button").classList.remove("loading");
+
+  if (response.ok) {
+    const data = await response.json();
+    responseRenderArea.textContent = data.server_response || "No response";
+  } else {
+    responseRenderArea.textContent = "Error communicating with server.";
+  }
+});
